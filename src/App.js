c148e680e5727351f90b7a6458914b510c30d14b
Maria Klochkova <56316185+MariaKlo@users.noreply.github.com>
2021-06-20 20:33:15 +0300

    Updating CSS files, App.js
    Trying to add images to the app (still in process)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import { useEffect, useState } from 'react';
-import win from './img/win.png';
-import lose from './img/lose.png';
-import draw from './img/draw.png';
-import rock from './img/rock.png';
-import paper from './img/paper.png';
-import scissors from './img/scissors.png';
+// import win from './img/win.png';
+// import lose from './img/lose.png';
+// import draw from './img/draw.png';
+// import rock from './img/rock.png';
+// import paper from './img/paper.png';
+// import scissors from './img/scissors.png';
 const App = () => {
     //set null if the user don't provide valid value
     const [userChoice, setUserChoice] = useState(null);
     const [computerChoice, setComputerChoice] = useState(null);
     const [result, setResult] = useState(null);
-    const [gameImage, setGameImages] = useState(null);
+    // const [gameImage, setGameImages] = useState(null);
     //create an array of choices
     const choices = ['rock', 'paper', 'scissors'];
     //create an array of images
-    const gameImages = [rock, paper, scissors];
-    const resultImages = [win, lose, draw];
+    // const gameImages = [rock, paper, scissors];
+    // const resultImages = [win, lose, draw];
+    
     //take value from user and print it on the screen
     const handleClick = (value) => {
         setUserChoice(value);
         generateComputerChoice();
-        generateGameImages();
     }
 
     //generate computer choice
@@ -29,24 +29,53 @@ const App = () => {
         setComputerChoice(randomChoice);
     }
 
-    //generate result pictures
-    const generateResultImages = () => {
+    //CREATE STATIC IMAGES IN JSX LIKE ONE RESULT AND ONE GAME
 
-    }
+    //create counter function
+    // const counterForUser = (count) => {
+        // useEffect(() => {
+        //     switch(userChoice) {
+        //         case 'scissorspaper':
+        //         case 'rockscissors':
+        //         case 'paperrock':
+        //             setResult(count++)
+        //             break;
+        //         case 'paperscissors':
+        //         case 'scissorsrock':
+        //         case 'rockpaper':
+        //             setResult(count--)
+        //             break;
+        //         case 'rockrock':
+        //         case 'paperpaper':
+        //         case 'scissorsscissors':
+        //             setResult(count)
+        //             break;
+        //     }
+        // }, [userChoice])
+    // }
+
+    //generate result pictures
+    // const generateResultImages = () => {
+
+    // }
 
     //generate game pictures
-    const generateGameImages = (value) => {
-        if (value === 'rock') {
-            return gameImages[0];
-        } else if (value === 'paper') {
-            return gameImages[1];
-        } else if (value === 'scissors') {
-            return gameImages[2];
-        } else {
-            return false;
-        }
-        setGameImages(value);
-    }
+    // const generateGameImages = (value) => {
+        //I need to change pic by the value of input. Check it out later
+    
+//         switch(value) {
+//         case 'rock':
+//             value = rock; break;
+//         case 'paper':
+//             value = paper; break;
+//         case 'scissors':
+//             value = scissors; break;
+        
+//     }
+//     setGameImages(value);
+// }
+
+    
 
     //check the result when computer&user choice changes
     useEffect(() => {
@@ -72,11 +101,13 @@ const App = () => {
     return (
         //the output on the screen
         <div>
+            <div className="counter-wrapper">
+                <p>You: </p>
+                <p>Computer: </p>
+            </div>
             <div className="header-wrapper">
-                <h1>User choice: {userChoice}{gameImage}</h1>
-                <img src={scissors} alt="scissors" className="user-img"/>
+                <h1>User choice: {userChoice}</h1>
                 <h1>Computer choice: {computerChoice}</h1>
-                <img src={scissors} alt="scissors" className="computer-img"/>
             </div> 
             <h1>Choose an option:</h1>
             <div className="button-wrapper">
@@ -85,7 +116,6 @@ const App = () => {
                 )}
             </div>
             <h1>{result}</h1>
-            <img src={win} alt="winner" className="result-img"/>
          </div>
     );
 }
